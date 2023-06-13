dd66e127bac3c67c622af96adbac152afc95237b
erikleisinger@gmail.com <erikleisinger>
2023-06-13 19:03:34 -0400

    cleanup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,21 +4,12 @@ import tables from '../database/tables'
 export const connection = new Connection(new Worker('src/scripts/jsstore.worker.js'));
 
 export const init = async () => {
-    console.log('INIT')
     var dbName ='JsStore_Demo';
     var database = {
         name: dbName,
         tables,
     }
     
-    const isDbCreated = await connection.initDb(database);
-    if(isDbCreated === true){
-        console.log("db created");
-        // here you can prefill database with some data
-    }
-    else {
-        console.log("db opened");
-        console.log(connection)
-    }
+    await connection.initDb(database);
     
 }
